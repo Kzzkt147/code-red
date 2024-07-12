4c071dc961bd511f838bb18c7a71145bc0ef6b19
Eli <79820324+Kzzkt147@users.noreply.github.com>
2024-07-12 13:49:20 +1200

    Add extra player animations
    Added walking and jumping animations.

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 @export var acceleration = 100.0
 @export var friction = 100.0
 
+@onready var animation = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -22,9 +24,12 @@ func _physics_process(delta):
 func handle_movement():
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("left", "right")
+	handle_direction(direction)
 	if direction:
+		if is_on_floor(): animation.play("walk")
 		velocity.x = move_toward(velocity.x, max_speed* direction, acceleration)
 	else:
+		if is_on_floor(): animation.play("idle")
 		velocity.x = move_toward(velocity.x, 0, friction)
 
 func apply_gravity(delta):
@@ -32,4 +37,11 @@ func apply_gravity(delta):
 
 func handle_jump():
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		animation.play("jump")
 		velocity.y = jump_velocity
+
+func handle_direction(direction):
+	if direction < 0:
+		animation.flip_h = true
+	elif direction > 0:
+		animation.flip_h = false
