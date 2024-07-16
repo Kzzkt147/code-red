b191a96ed2d4ac097f7d433f4c1105bd5964cca5
Eli <79820324+Kzzkt147@users.noreply.github.com>
2024-07-16 21:39:36 +1200

    Make camera script dynamic to viewport size

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -2,22 +2,21 @@ extends Camera2D
 
 @export var target : Node2D
 
-const CAMERA_WIDTH := 230
-
+@onready var camera_width : float = ProjectSettings.get_setting("display/window/size/viewport_width") / zoom.x
 var next_slide_x_value := 0
 var previous_slide_x_value := 0
 
 
 func _ready():
-	next_slide_x_value = CAMERA_WIDTH
+	next_slide_x_value = camera_width
 
 
 func _process(_delta):
 	if target.global_position.x > next_slide_x_value:
-		next_slide_x_value += CAMERA_WIDTH
-		previous_slide_x_value += CAMERA_WIDTH
-		global_position.x += CAMERA_WIDTH
+		next_slide_x_value += camera_width
+		previous_slide_x_value += camera_width
+		global_position.x += camera_width
 	elif target.global_position.x < previous_slide_x_value:
-		next_slide_x_value -= CAMERA_WIDTH
-		previous_slide_x_value -= CAMERA_WIDTH
-		global_position.x -= CAMERA_WIDTH
+		next_slide_x_value -= camera_width
+		previous_slide_x_value -= camera_width
+		global_position.x -= camera_width
