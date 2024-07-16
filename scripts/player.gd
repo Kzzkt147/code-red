6ba94f86bceacada18f21b3e5728160546b0b0ea
Kzzkt147 <e.p.borgman@gmail.com>
2024-07-16 15:27:16 +1200

    Add coyote jump. TODO: Limit jump to only once per floor touch.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,26 +1,36 @@
 extends CharacterBody2D
 
+# Player Paramaters
+@export_group("Player Movement Settings")
 @export var max_speed := 80.0
 @export var jump_velocity := -200.0
 @export var acceleration := 100.0
 @export var friction := 100.0
+@export_group("Player Jump Settings")
 @export var jump_buffer_timer := 0.1
+@export var coyote_time := 0.2
 
+# Node References
 @onready var animation := $AnimatedSprite2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+# Private variables
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var was_on_floor := true
 
 
 func _physics_process(delta) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		apply_gravity(delta)
+	else:
+		was_on_floor = true
 	
+	handle_coyote_time()
 	handle_movement()
 	handle_jump()
 
 	move_and_slide()
+
 
 func handle_movement() -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -33,23 +43,28 @@ func handle_movement() -> void:
 		if is_on_floor(): animation.play("idle")
 		velocity.x = move_toward(velocity.x, 0, friction)
 
+
 func apply_gravity(delta) -> void:
 	velocity.y += gravity * delta
 
+
 func handle_jump() -> void:
 	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
+		if is_on_floor() or was_on_floor:
 			jump()
 		else:
 			start_jump_buffer()
 
+
 func jump() -> void:
 	animation.play("jump")
 	velocity.y = jump_velocity
-	
+
+
 func on_jump_buffer_timeout() -> void:
 	if is_on_floor():
 		jump()
+
 
 func handle_direction(direction) -> void:
 	if direction < 0:
@@ -57,5 +72,14 @@ func handle_direction(direction) -> void:
 	elif direction > 0:
 		animation.flip_h = false
 
+
 func start_jump_buffer() -> void:
 	get_tree().create_timer(jump_buffer_timer).timeout.connect(on_jump_buffer_timeout)
+
+func handle_coyote_time() -> void:
+	if not is_on_floor() && was_on_floor:
+		get_tree().create_timer(coyote_time).timeout.connect(on_coyote_timeout)
+	pass
+	
+func on_coyote_timeout() -> void:
+	was_on_floor = false
